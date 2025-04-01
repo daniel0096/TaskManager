b4e629797cb3be3bba0e38bfcc03e8d6a1b4c4c6
daniel0096 <pokebalove@seznam.cz>
2025-04-01 16:18:16 +0200

    Update BasicDefines.h

--- src/BasicDefines.h
+++ src/BasicDefines.h
@@ -4,5 +4,16 @@
 /*if the application message output is associated with application failure/crash messages are still captured in non _DEBUG mode*/
 //#define _DEBUG
 
-//#define WIN32
-//#define __linux__
+#define APPLICATION_NAME "Task Manager"
+
+#if defined(_DEBUG)
+#	define APP_BUILD_MODE "Debug version"
+#else
+#	define APP_BUILD_MODE "Release version"
+#endif
+
+#ifdef __linux__
+#	define PLATFORM_NAME "Linux"
+#elif _WIN32
+#	define PLATFORM_NAME "Windows"
+#endif
